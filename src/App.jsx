1a4dcecc3606b3dff07addb0bcd7c66219ab1f5d
tortaruga <jojoway77@gmail.com>
2025-04-15 11:40:33 +0200

    updated repository link

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
       <footer>
         <p>This project uses <a href="https://github.com/cosinekitty/astronomy" target='_blank'>astronomy-engine</a> and <a href="https://www.npmjs.com/package/chinese-lunar-calendar" target='_blank'>chinese-lunar-calendar</a>.
-          If you're interestend in knowing more about how the calculators work check out <a href="">here</a>. </p>
+          If you're interestend in knowing more about how the calculators work check out <a href="https://github.com/tortaruga/chinese-new-year" target='_blank'>here</a>. </p>
           <p>coded by <a href="https://github.com/tortaruga" target='_blank'>tortaruga</a> ✨</p>
       </footer>
       
